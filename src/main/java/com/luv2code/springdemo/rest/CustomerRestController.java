53cde60fab49270c67eae1170bfac6ba007c2824
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-05-25 09:56:17 +0300

    Add controllers for updating and deleting the customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -32,9 +32,26 @@ public class CustomerRestController {
 
     @PostMapping("/customers")
     public Customer addCustomer(@RequestBody Customer customer) {
-        //in case the pass an id in JSON set id to 0, this will force save new item instead of update
+        // in case the pass an id in JSON set id to 0, this will force save new item instead of update
         customer.setId(0);
         customerService.saveCustomer(customer);
         return customer;
+    }
+
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId) {
+        Customer tmpCustomer = customerService.getCustomer(customerId);
+        if (tmpCustomer == null) {
+            throw new CustomerNotFoundException("Customer with id " + customerId + " not found");
+        }
+
+        customerService.deleteCustomer(customerId);
+        return "Deleted customer with id " + customerId;
     }
 }
